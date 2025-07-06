8ca7309df964304ffe46b62b35ee9d679891914e
Pablo Brasero Martínez <pablobrasero@gmail.com>
2025-07-06 23:06:25 +0200

    Cambios para login

--- PropieXpert/PropieXpert/PropieXpertApp.swift
+++ PropieXpert/PropieXpert/PropieXpertApp.swift
@@ -6,10 +6,23 @@
 //
 
 import SwiftUI
+import GoogleSignIn
 
 @main
 struct PropieXpertApp: App {
     @AppStorage("auth_token") var authToken: String = ""
+
+    init() {
+        // Inicialización manual de GoogleSignIn
+        if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+           let dict = NSDictionary(contentsOfFile: path),
+           let clientID = dict["CLIENT_ID"] as? String {
+            GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientID)
+            print("[GoogleSignIn] CLIENT_ID configurado manualmente: \(clientID)")
+        } else {
+            print("[GoogleSignIn] No se pudo encontrar CLIENT_ID en GoogleService-Info.plist")
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
