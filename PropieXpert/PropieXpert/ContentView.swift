651a06cf2b17b5e5415ac4129ed9774026b6d7c6
Pablo Brasero Martínez <pablobrasero@gmail.com>
2025-07-06 21:51:12 +0200

    Cambios para probar nueva app

--- PropieXpert/PropieXpert/ContentView.swift
+++ PropieXpert/PropieXpert/ContentView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            DashboardView()
+                .tabItem {
+                    Image(systemName: "rectangle.3.offgrid")
+                    Text("Dashboard")
+                }
+            PropertiesView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Propiedades")
+                }
+            FlowView()
+                .tabItem {
+                    Image(systemName: "arrow.left.arrow.right")
+                    Text("Flujo")
+                }
+            PlaceholderView()
+                .tabItem {
+                    Image(systemName: "ellipsis")
+                    Text("Más")
+                }
         }
-        .padding()
     }
 }
 
--- PropieXpert/PropieXpert/Views/PlaceholderView.swift
+++ PropieXpert/PropieXpert/Views/PlaceholderView.swift
@@ -13,5 +13,4 @@ struct PlaceholderView: View {
 
 #Preview {
     PlaceholderView()
-}
- 
+} 
