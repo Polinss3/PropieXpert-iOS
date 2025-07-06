55c17350070631424474addc1be7a2942773f380
Pablo Brasero <Pablo Brasero>
2025-07-06 21:46:07 +0200

    Cambios creada la app como tal

---
+++ PropieXpert/PropieXpert/PropieXpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropieXpertApp.swift
+//  PropieXpert
+//
+//  Created by Pablo Brasero Martínez on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PropieXpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PropieXpert/PropieXpertTests/PropieXpertTests.swift
@@ -0,0 +1,17 @@
+//
+//  PropieXpertTests.swift
+//  PropieXpertTests
+//
+//  Created by Pablo Brasero Martínez on 6/7/25.
+//
+
+import Testing
+@testable import PropieXpert
+
+struct PropieXpertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
