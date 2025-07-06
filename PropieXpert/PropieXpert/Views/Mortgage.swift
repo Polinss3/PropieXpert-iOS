af475f79965c4fc47a33471c67fb5a7865268f36
Pablo Brasero Martínez <pablobrasero@gmail.com>
2025-07-07 00:51:54 +0200

    cambios

--- PropieXpert/PropieXpert/Views/Mortgage.swift
+++ PropieXpert/PropieXpert/Views/Mortgage.swift
@@ -19,4 +19,51 @@ struct Mortgage: Identifiable, Codable {
     let description: String?
     let is_automatic_payment: Bool?
     // ... otros campos si los necesitas
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case property_id
+        case type
+        case initial_amount
+        case years
+        case interest_rate_fixed
+        case interest_rate_variable
+        case monthly_payment
+        case start_date
+        case end_date
+        case bank_name
+        case account_number
+        case total_to_pay
+        case payment_day
+        case fixed_rate_period
+        case reference_number
+        case description
+        case is_automatic_payment
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        // Intenta decodificar 'id', si no existe usa 'property_id'
+        if let id = try? container.decode(String.self, forKey: .id) {
+            self.id = id
+        } else {
+            self.id = try container.decode(String.self, forKey: .property_id)
+        }
+        self.type = try container.decode(String.self, forKey: .type)
+        self.initial_amount = try container.decode(Double.self, forKey: .initial_amount)
+        self.years = try container.decode(Int.self, forKey: .years)
+        self.interest_rate_fixed = try? container.decode(Double.self, forKey: .interest_rate_fixed)
+        self.interest_rate_variable = try? container.decode(Double.self, forKey: .interest_rate_variable)
+        self.monthly_payment = try container.decode(Double.self, forKey: .monthly_payment)
+        self.start_date = try? container.decode(String.self, forKey: .start_date)
+        self.end_date = try? container.decode(String.self, forKey: .end_date)
+        self.bank_name = try? container.decode(String.self, forKey: .bank_name)
+        self.account_number = try? container.decode(String.self, forKey: .account_number)
+        self.total_to_pay = try? container.decode(Double.self, forKey: .total_to_pay)
+        self.payment_day = try? container.decode(Int.self, forKey: .payment_day)
+        self.fixed_rate_period = try? container.decode(Int.self, forKey: .fixed_rate_period)
+        self.reference_number = try? container.decode(String.self, forKey: .reference_number)
+        self.description = try? container.decode(String.self, forKey: .description)
+        self.is_automatic_payment = try? container.decode(Bool.self, forKey: .is_automatic_payment)
+    }
 } 
